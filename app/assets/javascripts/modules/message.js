861e8f47c8e8da24c3df3093d719547ec30efb66
shokasuya <shou2379@gmail.com>
2020-06-19 22:00:12 +0900

    自動更新機能

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,8 +1,47 @@
 $(function(){
+  console.log("ok")
+
+//   let reloadMessages = function() {
+//     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+//   let last_message_id = $('.chat-main__message-list__box:last').data("message-id");
+//   console.log(last_message_id)
+
+//   $.ajax({
+//     //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+//     url: "api/messages",
+//     //ルーティングで設定した通りhttpメソッドをgetに指定
+//     type: 'get',
+//     dataType: 'json',
+//     //dataオプションでリクエストに値を含める
+//     data: {id: last_message_id}
+//   })
+//   .done(function(messages) {
+//     // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+//     if (messages.length !== 0) {
+//       //追加するHTMLの入れ物を作る
+//       let insertHTML = '';
+//       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+//       $.each(messages, function(i, message) {
+//         insertHTML += buildHTML(message)
+//       });
+//       //メッセージが入ったHTMLに、入れ物ごと追加
+//       $('.MessageField').append(insertHTML);
+//       $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+
+//     }
+//   })
+//   .fail(function() {
+//     alert('error');
+//   });
+// };
+// setInterval(reloadMessages, 7000);
+
+
+
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="chat-main__message-list__box">
+        `<div class="chat-main__message-list__box" data-message-id=${message.id}>
           <div class="chat-main__message-list__box__message-info">
             <div class="chat-main__message-list__box__message-info__user-name">
               ${message.user_name}
@@ -21,7 +60,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="chat-main__message-list__box">
+      `<div class="chat-main__message-list__box" data-message-id=${message.id}>
         <div class="chat-main__message-list__box__message-info">
           <div class="chat-main__message-list__box__message-info__user-name">
             ${message.user_name}
@@ -54,7 +93,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      console.log(data)
+      // console.log(data)
       let html = buildHTML(data);
       $('.chat-main__message-list').append(html);      
       $('form')[0].reset();
@@ -68,4 +107,5 @@ $(function(){
       $('.input__submit').removeAttr('disabled');
     });
   })
+  
 });
